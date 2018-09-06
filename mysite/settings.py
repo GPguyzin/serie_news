65223e9a58136da7f78379015d1d2aa9a81286b6
Guilherme <guilhermemx17.go@gmail.com>
2018-09-06 15:39:50 -0300

    fazendo tela de login para manuseio do site e botão publicar e excluir

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-
+LOGIN_REDIRECT_URL = '/'
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -24,6 +24,7 @@ SECRET_KEY = '%dldlov+k-xd4o+4@jd6-^oc=z%$#j_+f(kz43t@j9pho-5j%c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
@@ -120,3 +121,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+LOGIN_REDIRECT_URL = '/'
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
 from django.contrib import admin
 
+from django.contrib.auth import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    path(r'^admin/', admin.site.urls),
+    path(r'^accounts/login/$', views.login, name='login'),
+    path(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
+    path(r'', include('blog.urls')),
 ]
