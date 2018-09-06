7e54151e7085d83a5624fecc66f54a0597822499
Guilherme <guilhermemx17.go@gmail.com>
2018-09-06 16:18:51 -0300

    Fazendo a parte de comentarios

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 from django.contrib import admin
-
 from django.contrib.auth import views
 
 urlpatterns = [
-    path(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path(r'^accounts/login/$', views.login, name='login'),
     path(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
     path(r'', include('blog.urls')),
 ]
+
+
+
+
